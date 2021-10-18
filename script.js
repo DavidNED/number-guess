a931c9d9a24f59b566f0b49d2ae19cdda5602e63
DavidNED <d_middelveld@hotmail.com>
2021-10-19 00:08:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,182 @@
+const btnCheck = document.querySelector('.check');
+const btnAgain = document.querySelector('.again');
+const btnSumbit = document.querySelector('.btn-submit');
+const guess = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const messageError = document.querySelector('.message-error');
+const scoreLabel = document.querySelector('.score');
+const highScoreLabel = document.querySelector('.highscore');
+const minNum = document.querySelector('.min-num');
+const maxNum = document.querySelector('.max-num');
+const between = document.querySelector('.between');
+
+const state = {
+  secretNum: 0,
+  highScore: 0,
+  score: 20,
+  maxNum: 20,
+  minNum: 1,
+};
+
+window.s = state;
+
+// POINTS FOR 19-10:
+// 1. WHEN FILLING IN 20 AT MAX AND 30 AT MIN, SECRETNUMBER IS STILL UPDATED
+// 2. WITH ERRORS AT INPUT FIELD USER CAN STILL CLICK THE CHECK BUTTON
+
+//------------------------------------------------------------------
+
+const checkMaxGreater = function (negativeVal = '') {
+  // Check if min. number is greater than max. number.
+  // If this is the case, return; min. number should always be lower.
+
+  // With only negative values (negativeVal), things work different with comparing
+  const condition = negativeVal
+    ? state.minNum < state.maxNum
+    : state.minNum > state.maxNum;
+
+  if (condition) {
+    messageError.textContent =
+      "Minimum number can't be higher than maximum number.";
+    state.maxNum = 20;
+    state.minNum = 1;
+    state.secretNum = 0;
+
+    setTimeout(() => {
+      messageError.textContent = '';
+    }, 3500);
+
+    return;
+  }
+};
+
+const checkInput = function () {
+  // Set borders back to default - when user filled in one field before instead of two the border was red
+  minNum.style.border = '2px solid #eee';
+  maxNum.style.border = '2px solid #eee';
+
+  // Get values out of fields
+  const maxNumVal = maxNum.value;
+  const minNumVal = minNum.value;
+
+  // When both fields are filled in, move on to init() function
+  if (maxNumVal !== '' || minNumVal !== '') {
+    state.maxNum = +maxNumVal;
+    state.minNum = +minNumVal;
+  }
+
+  // When one of the fields is empty, apply red border to that field
+  if (maxNumVal === '' || minNumVal === '') {
+    minNum.value === ''
+      ? (minNum.style.border = '2px solid #C02B0B')
+      : (maxNum.style.border = '2px solid #C02B0B');
+
+    // Set back to 20 and 1 (in this case either one of them is set to 0 because of line 51 and 52)
+    state.maxNum = 20;
+    state.minNum = 1;
+    state.secretNum = 0;
+
+    return;
+  }
+
+  init();
+};
+
+//------------------------------------------------------------------
+
+const init = function () {
+  // Return when both values are the same
+  if (state.maxNum === state.minNum) {
+    messageError.textContent = "Numbers can't be the same.";
+    state.maxNum = 20;
+    state.minNum = 1;
+    state.secretNum = 0;
+    return;
+  }
+
+  // When both min. and max. values are positive (default)
+  if (state.maxNum > 0 && state.minNum > 0) {
+    checkMaxGreater();
+    state.secretNum =
+      Math.trunc(Math.random() * (state.maxNum - state.minNum) + 1) +
+      state.minNum;
+  }
+
+  // When both min. and max. numbers are negative
+  // With only negative values (negativeVal), things work the other way around with comparing
+  if (state.maxNum < 0 && state.minNum < 0) {
+    checkMaxGreater('negativeVal');
+    state.secretNum =
+      Math.trunc(Math.random() * (state.maxNum - state.minNum) - 1) +
+      state.minNum;
+    return;
+  }
+
+  // When one of the numbers is positive and one negative
+  if (state.maxNum < 0 || state.minNum < 0) {
+    checkMaxGreater();
+
+    // e.g. max = 30, min = -20. Player basically wants a number between 1 to 30 and -1 to -20.
+    // We create a random number. When it is <= the minimum number (in this case 20 (we cut of the '-')), we create a random number again
+    // When this random number is > 0.5 we multiply the first created random number 1, when it is < 0.5 we multiple it by -1
+    // With this we can create a number that is either negative or positive when the number is <= 20 (and in this example this is what the user wants (number between -1 to -20))
+    // In this case -20 is excluded and -1 is included
+    let num = Math.trunc(Math.random() * state.maxNum) + 1;
+    if (num <= Math.abs(state.minNum)) {
+      num *= Math.round() > 0.5 ? 1 : -1;
+    }
+    state.secretNum = num;
+  }
+
+  state.score = 20;
+  scoreLabel.textContent = '20';
+  message.textContent = 'Start guessing...';
+  guess.value = '';
+  minNum.value = '';
+  maxNum.value = '';
+  between.textContent = `(Between ${state.minNum} and ${state.maxNum})`;
+};
+
+//------------------------------------------------------------------
+
+// Event listeners
+btnCheck.addEventListener('click', function () {
+  const val = +guess.value;
+
+  if (val > state.maxNum || val < state.minNum) {
+    message.textContent = `Number range is ${state.minNum}-${state.maxNum}.`;
+    return;
+  }
+
+  if (val > state.secretNum) {
+    state.score--;
+    message.textContent = 'Too high';
+    scoreLabel.textContent = state.score;
+  }
+
+  if (val < state.secretNum) {
+    state.score--;
+    message.textContent = 'Too low';
+    scoreLabel.textContent = state.score;
+  }
+
+  if (val === state.secretNum) {
+    state.score--;
+    message.textContent = 'Correct!';
+    if (state.score > state.highScore) state.highScore = state.score;
+    scoreLabel.textContent = state.score;
+    highScoreLabel.textContent = state.highScore;
+  }
+});
+
+const elements = [minNum, maxNum];
+elements.forEach(element =>
+  element.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') checkInput();
+  })
+);
+
+btnSumbit.addEventListener('click', checkInput);
+btnAgain.addEventListener('click', init);
+
+init();
